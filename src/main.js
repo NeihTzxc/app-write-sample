20271f39d6a89321e66b69a61d63c3425208602e
NeihTzxc <45267625+NeihTzxc@users.noreply.github.com>
2024-09-27 13:32:15 +0700

    remove using route.use

--- src/main.js
+++ src/main.js
@@ -3,7 +3,13 @@ import userRoutes from './routers/user.js'
 import authMiddleware from "./middleware/auth.js"
 const app = new AppExpress()
 
-app.middleware(authMiddleware)
-app.use("/user", userRoutes)
+const getRouters = (req, res) => {
+    res.json({
+        'routers': ['/', '/user/:username', '/user']
+    })
+}
+
+app.get('/', getRouters)
+// app.use("/user", userRoutes)
 
 export default async (context) => await app.attach(context);
