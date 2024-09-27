2af6d2c4be4c4a2cf7bfda48dd4942225d566a56
NeihTzxc <45267625+NeihTzxc@users.noreply.github.com>
2024-09-27 13:41:17 +0700

    getUsers

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import AppExpress from "@itznotabug/appexpress"
-import userRoutes from './routers/user.js'
 import authMiddleware from "./middleware/auth.js"
 const app = new AppExpress()
+import { Client, Users } from 'node-appwrite';
 
 const getRouters = (req, res) => {
     res.json({
@@ -9,7 +9,23 @@ const getRouters = (req, res) => {
     })
 }
 
+const getUsers = (req, res, log, error) => {
+    const client = new Client()
+    .setEndpoint(process.env.APPWRITE_FUNCTION_API_ENDPOINT)
+    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
+    .setKey(req.headers['x-appwrite-key'] ?? '');
+    const users = new Users(client);
+    const response = users.list();
+    log(`Response: ${response}`)
+    res.json({
+        'users': response
+    })
+
+}
+
 app.get('/', getRouters)
 // app.use("/user", userRoutes)
+
+app.get('/users', getUsers)
 
 export default async (context) => await app.attach(context);
