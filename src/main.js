a3d62ffad66b3567aff3250e58f60f820a089d2c
NeihTzxc <45267625+NeihTzxc@users.noreply.github.com>
2024-09-27 11:45:50 +0700

    middleware

--- src/main.js
+++ src/main.js
@@ -1,28 +1,10 @@
 import AppExpress from "@itznotabug/appexpress"
-
+import userRoutes from './routers/user.js'
+import authMiddleware from "./middleware/auth.js"
 const app = new AppExpress()
 
-const getRouters = (req, res) => {
-    res.json({
-        'routers': ['/', '/user/:username', '/user']
-    })
-}
-
-const getUser = (req, res) => {
-    const { username } = req.params 
-    res.json({
-        'username': username
-    })
-}
-
-const createUser = (req, res) => {
-    res.json({
-        'postData': req.body
-    }) 
-}
-
+app.middleware(authMiddleware)
 app.get('/', getRouters)
-app.get('/user/:username', getUser)
-app.post("/user", createUser)
+app.use("/user", userRoutes)
 
 export default async (context) => await app.attach(context);
---
+++ src/routers/user.js
@@ -0,0 +1,19 @@
+import AppExpress from "@itznotabug/appexpress"
+
+const getUser = (req, res) => {
+    const { username } = req.params 
+    res.json({
+        'username': username
+    })
+}
+
+const createUser = (req, res) => {
+    res.json({
+        'postData': req.body
+    }) 
+}
+
+app.get('/user/:username', getUser)
+app.post("/user", createUser)
+
+export default router
